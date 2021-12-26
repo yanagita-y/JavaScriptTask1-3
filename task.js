02650773f3c0c600464736512f5899a27cf0fd4b
DESKTOP-6LCURLL\Yanagita <yosiphi@hotmail.com>
2021-12-27 02:31:20 +0900

    2021/12/27 再レビュー願い

--- task.js
+++ task.js
@@ -1,17 +1,19 @@
-let arr = new Array();
-arr[0]='掃除';
-arr[1]='買い物';
-arr[2]='散歩';
+const arr=['掃除','買い物','散歩'];
 
-const task = () => {
+const out = () => {
     console.log('========================');
     console.log('現在持っているタスク一覧');
     console.log('========================');
     for(let i=0 ; i < arr.length ; i++) {
         console.log(`${i} : ${arr[i]}`);
     }
-    arr[arr.length] = prompt("タスクを入力してください");
+}
+
+const task = () => {
+    let str = prompt("タスクを入力してください")
+    arr[arr.length] = str;
     alert("新しいタスクを追加しました。");
+    out();
     prompt("「確認,追加,削除,終了」の４つのいずれかを入力してください");
     task();
 }
