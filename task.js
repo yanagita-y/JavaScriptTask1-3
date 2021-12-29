99f64ecab3dac591095b5b919b2b3cf0b058c2fd
DESKTOP-6LCURLL\Yanagita <yosiphi@hotmail.com>
2021-12-29 09:47:08 +0900

    2021/12/29修正

--- task.js
+++ task.js
@@ -6,7 +6,7 @@ const tasklist=['掃除','買い物','散歩'];
 const out = () => {
     console.log(title);
     tasklist.forEach((element, index) => console.log(`${index} : ${element}`));
-//上記記載方法で、参考画像のとおり出力しようとしたものの、アポストロフィを表示しないやり方を思いつけなかった
+//上記記載方法で、参考画像のとおり出力しようとしたものの、アポストロフィを表示しないやり方を思いつけなかった⇒できた
 /*    for(let i=0 ; i < tasklist.length ; i++) {
         console.log(`${i} : ${tasklist[i]}`);
     } */
