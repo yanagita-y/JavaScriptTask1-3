66e3475b7e2c433f452e5a3ac2768456c43e42b9
DESKTOP-6LCURLL\Yanagita <yosiphi@hotmail.com>
2022-01-01 18:18:54 +0900

    2022/1/1修正

--- task.js
+++ task.js
@@ -5,14 +5,11 @@ const tasklist=['掃除','買い物','散歩'];
 
 const out = () => {
     console.log(title);
-    tasklist.forEach((element, index) => console.log(`${index} : ${element}`));
-/*    for(let i=0 ; i < tasklist.length ; i++) {
-        console.log(`${i} : ${tasklist[i]}`);
-    } */
+    tasklist.forEach((task, index) => console.log(`${index} : ${task}`));
 }
 
 const task = () => {
-    let job = prompt('タスクを入力してください。')
+    let job = prompt('タスクを入力してください。');
     tasklist.push(job);
     alert('新しいタスクを追加しました。');
     out();
